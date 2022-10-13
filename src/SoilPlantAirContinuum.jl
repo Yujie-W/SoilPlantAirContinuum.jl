7b07d20db4c9bca0343ff29d9d4ef13b7da51c31
Yujie Wang <jesiner@gmail.com>
2022-10-12 22:16:15 -0700

    pass test

--- src/SoilPlantAirContinuum.jl
+++ src/SoilPlantAirContinuum.jl
@@ -20,8 +20,8 @@ using PlantHydraulics: AbstractPlantOrganism, GrassLikeOrganism,
       create_grass, critical_flow, end_pressure, flow_profile!,
       pressure_profile!, roots_flow!, soil_p_25_swc, soil_swc,
       temperature_effects!
-using StomataModels: AbstractStomatalModel, BetaGLinearPsoil, CanopyLayer, ESMBallBerry, ESMMedlyn, GswDrive, OptimizationStomatalModel, gas_exchange!, gsw_control!, prognostic_gsw!,
-      stomatal_conductance, update_leaf_TP!, β_factor
+using StomataModels: AbstractStomatalModel, BetaGLinearPsoil, CanopyLayer, ESMBallBerry, ESMMedlyn, EmpiricalStomatalModel, GswDrive, OptimizationStomatalModel, gas_exchange!, gsw_control!,
+      prognostic_gsw!, stomatal_conductance, update_leaf_TP!, β_factor
 using UnPack: @unpack
 using WaterPhysics: latent_heat_vapor, relative_diffusive_coefficient,
       saturation_vapor_pressure
